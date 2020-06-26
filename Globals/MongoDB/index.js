0ea8f920bc19c2c45f619631157ba4a691a51fe8
akmusham <akmusham@gmail.com>
2020-06-27 01:20:08 +0530

    mongodb connectionstr

--- Globals/MongoDB/index.js
+++ Globals/MongoDB/index.js
@@ -5,7 +5,7 @@ global.connectedEmitter = new eventEmitter()
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-global.db = mongoose.createConnection(`mongodb+srv://akhil:UMQt3M9PcFEHNWKh@cluster0-vp4jb.mongodb.net/contentApp`,{ useNewUrlParser: true, useUnifiedTopology: true })
+global.db = mongoose.createConnection(process.env.mongodbConnectionurl,{ useNewUrlParser: true, useUnifiedTopology: true })
 db.on('connected', () => {
   console.log('--------------Connected to DB----------------')
   connectedEmitter.emit('connectedMongoDB')
